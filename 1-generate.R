6f041f68b1ef22e7c8a6b1100375e3550711d276
Jayden Nord <jaydennord@gmail.com>
2020-01-27 16:11:46 -0600

    updated conditions

--- 1-generate.R
+++ 1-generate.R
@@ -23,35 +23,35 @@ gen_data <- function(id, nblk, blk_sd, gen_method, phi, eu_sd, mu, ...) {
     m10 = rep(10, 6),
     m45 = rep(45, 6)
   )
-  
+
   neu <- length(mu)
   n <- nblk * neu
-  
+
   blk <- rep(seq_len(nblk), each = neu)
   trt  <- rep_len(seq_len(neu), n)
   mu <- mu[trt]
-  
+
   b <- rnorm(n = nblk, sd = blk_sd)[blk]
-  
+
   y <- switch(
-    
+
     EXPR = gen_method,
-    
+
     pois_normal = {
       e <- rnorm(n, sd = eu_sd)
       eta <- log(mu) + b + e
       lambda <- exp(eta)
       rpois(n, lambda)
     },
-    
+
     pois_gamma = {
       eta <- log(mu) + b
       lambda <- exp(eta)
       rnbinom(n, lambda, phi)
     }
-    
+
   )
-  
+
   return(data.frame(
     id  = id,
     blk = factor(blk),
@@ -59,8 +59,8 @@ gen_data <- function(id, nblk, blk_sd, gen_method, phi, eu_sd, mu, ...) {
     y   = y,
     stringsAsFactors = FALSE
   ))
-  
-  
+
+
 }
 
 # design matrix -----------------------------------------------------------
@@ -72,37 +72,38 @@ gen_data <- function(id, nblk, blk_sd, gen_method, phi, eu_sd, mu, ...) {
 dsn_common <- crossing(
   nblk = c(4, 20),
   neu = 6,
-  blk_sd = .5,
-  #mu = list(
-  #  rep(10, 6),
-  #  rep(45, 6)
-  #),
+  blk_sd = c(.1, .25, .5),
   mu = c("m10", "m45"),
   rep = 1:nrep
 )
 
-dsn_methods <- list(
-  
-  pois_gamma = crossing(
-    # phi = c(.1, .2)
-    phi = NULL
-  ),
-  
-  pois_normal = crossing(
-    eu_sd = .5
-  )
-  
-) %>% bind_rows(.id = "gen_method")
+# dsn_methods <- list(
+# 
+#   pois_gamma = crossing(
+#     phi = c(.1, .2)
+#   ),
+# 
+#   pois_normal = crossing(
+#     eu_sd = .5
+#   )
+# 
+# ) %>% bind_rows(.id = "gen_method")
 
 
-set.seed(12478)
+set.seed(12479)
 
-dsn <- crossing(dsn_common, dsn_methods) %>%
+# dsn <- crossing(dsn_common, dsn_methods) %>%
+#   mutate(
+#     id = stri_rand_strings(n(), length = 10),
+#     sim_block = sample(0:749, size = n(), replace = TRUE)
+#   )
+dsn <- dsn_common %>% crossing(
+  gen_method = c("pois_gamma", "pois_normal")
+) %>%
   mutate(
-    id = stri_rand_strings(n(), length = 10),
-    sim_block = sample(0:749, size = n(), replace = TRUE)
+    eu_sd = sqrt(1 - blk_sd^2),
+    phi = 1 / (exp(eu_sd^2 / 2) + 1)
   )
-
 
 
 # generate ----------------------------------------------------------------
@@ -118,4 +119,3 @@ dsn %>%
     d <- pmap_dfr(.x, gen_data)
     fwrite(d, file = paste0("data/data_", .y, ".csv"), sep = ",")
   })
-
