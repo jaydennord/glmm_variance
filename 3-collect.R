8852f363ddbbdfd0e8bba110af867e3f4e121cd0
Jayden Nord <jaydennord@gmail.com>
2020-01-31 13:33:40 -0600

    updated collection script

--- 3-collect.R
+++ 3-collect.R
@@ -10,21 +10,41 @@ devtools::session_info()
 
 # collection --------------------------------------------------------------
 
-from_r <- paste0("res/res_", 0:749, ".csv") %>%
-  map_dfr(read_csv, col_types = cols())
+# from_r <- paste0("res/res_", 0:749, ".csv") %>%
+#   map_dfr(read_csv, col_types = cols())
+# 
+# from_sas <- dir(path = "res", "wald_", full.names = TRUE) %>%
+#   map_dfr(read_csv, col_types = cols(), .id = "source") %>%
+#   filter(CovParm == "Intercept")
 
-from_sas <- dir(path = "res", "wald_", full.names = TRUE) %>%
-  map_dfr(read_csv, col_types = cols()) %>%
-  select(id, c025 = LowerWaldCL, c975 = UpperWaldCL)
 
-results <- bind_rows(from_r, from_sas, .id = "source") %>%
+results <- c("res", "wald_normal", "wald_nb") %>%
+  set_names() %>%
+  map(~ 
+    paste0("res/", ., "_", 0:749, ".csv") %>%
+    map_dfr(read_csv, col_types = cols())
+  ) %>%
+  modify_at(
+    vars(starts_with("wald_")), 
+    ~ select(., id, c025 = LowerWaldCL, c975 = UpperWaldCL)
+  ) %>%
+  bind_rows(.id = "source") %>%
   mutate(
     ana_method = if_else(is.na(ana_method), source, ana_method),
     source = NULL
   )
 
+# %>%
+#   select(id, c025 = LowerWaldCL, c975 = UpperWaldCL)
+
+# results <- bind_rows(from_r, from_sas, .id = "source") %>%
+#   mutate(
+#     ana_method = if_else(is.na(ana_method), source, ana_method),
+#     source = NULL
+#   )
+
 dsn <- read_csv("dsn.csv")
 
-d <- left_join(results, dsn, by = "id")
+d <- right_join(dsn, results, by = "id")
 
-write_csv(d, "results.csv")
+write_csv(d, "results_full.csv")
