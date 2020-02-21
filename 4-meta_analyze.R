cf28f877a28a0f7a9801bfc12e4491f322cc3f1b
jaydennord <jaydennord@gmail.com>
2020-02-21 01:56:55 -0600

    updated results and exploring

--- 4-meta_analyze.R
+++ 4-meta_analyze.R
@@ -25,6 +25,42 @@ d %>% group_by(!!! grps) %>% count() %>% View
 #   geom_bar(stat = "identity") +
 #   facet_grid(nblk + blk_sd ~ mu + phi)
 
+d2 <- d %>%
+  mutate(
+    bias = (estimate - blk_sd) / blk_sd
+  ) %>% 
+  filter(
+    !is.na(c025) & !is.na(c975),
+    abs(bias) < 1,
+    str_detect(ana_method, "wald_", negate = TRUE) & is.na(error) |
+      str_detect(ana_method, "wald_") & sas_status == 0 & sas_g_mat == 1
+  ) 
+
+d2 %>%
+  ggplot(aes(x = bias)) + 
+  geom_histogram() + 
+  facet_grid(nblk + blk_sd ~ mu + gen_method + ana_method) +
+  geom_vline(xintercept = 0, color = "blue")
+
+d3 <- d2 %>%
+  group_by(!!! grps) %>%
+  summarize(
+    m = mean(bias, na.rm = TRUE),
+    lo = m - 1.96 * sd(bias) / sqrt(n()),
+    hi = m + 1.96 * sd(bias) / sqrt(n())
+  )
+
+d3 %>%
+  ggplot(aes(x = ana_method, y = m, ymin = lo, ymax = hi)) +
+  geom_rect(ymin = .05, ymax = Inf, xmin = -Inf, xmax = Inf, color = "gray", alpha = .1) +
+  geom_rect(ymin = -Inf, ymax = -.05, xmin = -Inf, xmax = Inf, color = "gray", alpha = .1) +
+  geom_point() + 
+  geom_errorbar() +
+  facet_grid(nblk + blk_sd ~ mu + gen_method) +
+  geom_hline(yintercept = 0, color = "blue", alpha = .25)
+  
+
+
 d %>% filter(c975 < 5) %>%
 ggplot(aes(x = c975)) + 
   geom_histogram() +
