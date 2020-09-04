ced58822dbce3fbc6aa89d3799fae58ccf258953
jaydennord <jaydennord@gmail.com>
2020-09-04 17:01:12 -0500

    removed email
    Removing email commands on the off-chance someone forks this

--- s2_analyze.sh
+++ s2_analyze.sh
@@ -6,8 +6,6 @@
 #SBATCH --job-name=ana
 #SBATCH --error=./slurm/job_%A_%a.err
 #SBATCH --output=./slurm/job_%A_%a.out
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=jaydennord@gmail.com
 
 module load R
 Rscript 2-analyze.R $SLURM_ARRAY_TASK_ID
